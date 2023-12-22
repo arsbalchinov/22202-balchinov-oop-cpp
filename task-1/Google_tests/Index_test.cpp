ada1bcc346deb8ed2ffc14cdc5a9698474a92c75
a.balchinov <a.balchinov@g.nsu.ru>
2023-12-22 22:33:01 +0700

    add const tests

--- task-1/Google_tests/Index_test.cpp
+++ task-1/Google_tests/Index_test.cpp
@@ -48,3 +48,26 @@ TEST(Index, BackTest3) {
     CircularBuffer cb;
     EXPECT_THROW(cb.back(), out_of_range);
 }
+TEST(Index, ConstOperatorTest) {
+    const CircularBuffer cb(3, 1);
+    EXPECT_EQ(cb[0], 1);
+}
+TEST(Index, ConstAtTest) {
+    const CircularBuffer cb(3, 1);
+    EXPECT_THROW(cb.at(-1), out_of_range);
+    EXPECT_THROW(cb.at(5), out_of_range);
+    EXPECT_THROW(cb.at(3), out_of_range);
+    EXPECT_EQ(cb.at(0), 1);
+    EXPECT_EQ(cb.at(1), 1);
+}
+TEST(Index, ConstFrontTest) {
+    const CircularBuffer cb(3, 1);
+    EXPECT_EQ(cb.front(), 1);
+    EXPECT_EQ(cb.at(0), 1);
+    EXPECT_EQ(cb.at(1), 1);
+}
+TEST(Index, ConstBackTest) {
+    const CircularBuffer cb(3, 1);
+    EXPECT_EQ(cb.at(2), 1);
+    EXPECT_EQ(cb.back(), 1);
+}
